80e22421847b5d6311812d35e9c6e2dd31d86c3e
Adrien Devresse <adrien.devresse@epfl.ch>
2015-06-16 14:15:17 +0200

    Disable -Werror by default, this causes falase positive and compialtion error with recent boost versions or clang as compiler

--- pkgs/bbp/config/bbp-cmake/default.nix
+++ pkgs/bbp/config/bbp-cmake/default.nix
@@ -5,14 +5,20 @@ stdenv.mkDerivation rec {
   
   
   src = fetchFromGitHub {
-	owner = "Eyescale";
-	repo = "CMake";
+  owner = "Eyescale";
+  repo = "CMake";
     rev = "0bd6baa5ef821409564ec2d16e268d68c6ef44a0";
     sha256 = "18vicw6f6ygzbixii26svv9jc286xmjhn78v10zl6zrcr49vxf06";
   };
 
+  
+  # enforce -WError for portable build is generally a bad practice
+  # this cause any build with clang or recent boost version to fail 
+  # due to external warnings
+  # suppress it
   installPhase = ''
-      mkdir -p $out/share/bbp-cmake/Common
+      sed -i 's@-Werror@@g' Compiler.cmake && 
+      mkdir -p $out/share/bbp-cmake/Common &&
       cp -R *  $out/share/bbp-cmake/Common
     '';
 
