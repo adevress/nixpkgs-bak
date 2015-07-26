1c8f620e42296ff5620a7bb4ef8671f5cb979a7d
Adrien Devresse <adrien.devresse@epfl.ch>
2015-07-26 21:27:22 +0200

    Upgrade HPCTools verison to 3.3.0

--- pkgs/bbp/hpc/hpctools/default.nix
+++ pkgs/bbp/hpc/hpctools/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchgitPrivate, boost, libxml2, cmake, bbp-cmake, mpiRuntime, pkgconfig, python, hdf5, doxygen }:
 
 stdenv.mkDerivation rec {
-  name = "hpctools-3.2.0-DEV";
+  name = "hpctools-3.3.0-stable";
   buildInputs = [ stdenv pkgconfig boost cmake mpiRuntime libxml2 python hdf5 doxygen];
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/hpc/HPCTools";
-    rev = "2778b16bef77a0f83b71aa8f89df79bfc726203a";
-    sha256 = "0b9kzr0wbh71lvnb45l7yzxv66s9s46b2733qndma32sf7wm2cxq";    
+    rev = "f6ab16612e33692ad057c5ea637a141c4a19378a";
+    sha256 = "0wbh15mnl7y4dj8g7ln6abagg9mjhkpvhlvii22r0xvmv86fzy1h";    
   };
   
   patchPhase= ''
