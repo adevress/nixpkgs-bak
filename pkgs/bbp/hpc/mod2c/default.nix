d8aa25a496cc07f178af8e3b7714b4bf45f34a15
Adrien Devresse <adrien.devresse@epfl.ch>
2015-07-27 14:31:22 +0200

    Update mod2c to 2015R2 release ( 1.1.1 )

--- pkgs/bbp/hpc/mod2c/default.nix
+++ pkgs/bbp/hpc/mod2c/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchgitPrivate, cmake, bbp-cmake, pkgconfig, bison, flex, python }:
 
 stdenv.mkDerivation rec {
-  name = "mod2c-1.1.1";
+  name = "mod2c-1.1.1-stable";
   buildInputs = [ stdenv cmake pkgconfig bison flex python];
 
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/sim/mod2c";
-    rev = "64ca3ee83bbaf9f80ea14e7f7fb2d9ab34577965";
-    sha256 = "1q12ld9jbjd0jhkgjxx9qbvykh2sc00kn14l2347xcjkcl2awckj";
+    rev = "2d8cbf240ed6739de6c06a397238e028f61bf307";
+    sha256 = "1i3xjyk1gq0vax07lkp48jhcx48dgf26q1yhjz5jh0vnbaarxvjz";
   };
   
   patchPhase= ''
