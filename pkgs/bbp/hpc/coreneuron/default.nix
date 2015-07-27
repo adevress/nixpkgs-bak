8e6534ce5009f38e19580bfb80a65d99bdfd7f0d
Adrien Devresse <adrien.devresse@epfl.ch>
2015-07-27 14:37:46 +0200

    Update coreneuron to 2015R2 release ( 0.60.0 )

--- pkgs/bbp/hpc/coreneuron/default.nix
+++ pkgs/bbp/hpc/coreneuron/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchgitPrivate, perl, cmake, bbp-cmake, pkgconfig, mpiRuntime, mod2c}:
 
 stdenv.mkDerivation rec {
-  name = "coreneuron-0.6.0-DEV";
+  name = "coreneuron-0.6.0-stable";
   buildInputs = [ stdenv perl cmake pkgconfig mpiRuntime mod2c];
 
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/sim/coreneuron";
-    rev = "cd01b52a7e5dd518678575a2e5b4c08dd5b27167";
-    sha256 = "0r5qcnp3xzb77q13vmslwjri84d0q3swrjd10zl7724sw97lzlmg";
+    rev = "85e93ec5f5648521a6c53186baf94a43849dea7a";
+    sha256 = "0kl1spf1f24qac0mxn57l075f9ifs03ia99byazyxzzprp3la0zx";
   };
   
   patchPhase= ''
