e7fe769f5b9eab789ee78a98f3d1a1d3efa2369f
Adrien Devresse <adrien.devresse@epfl.ch>
2015-07-27 21:52:06 +0200

    Add neurodamus support for 2015R2 ( 1.8.1 )

--- pkgs/bbp/hpc/neurodamus/default.nix
+++ pkgs/bbp/hpc/neurodamus/default.nix
@@ -1,8 +1,18 @@
-{ stdenv, fetchgitPrivate, cmake, bbp-cmake, pkgconfig, hdf5, mpiRuntime, bluron}:
+{ stdenv
+, fetchgitPrivate
+, cmake
+, bbp-cmake
+, pkgconfig
+, hdf5
+, mpiRuntime
+, zlib
+, ncurses
+, bluron
+, reportinglib}:
 
 stdenv.mkDerivation rec {
   name = "neurodamus-1.8.1-stable";
-  buildInputs = [ stdenv cmake pkgconfig hdf5 mpiRuntime bluron];
+  buildInputs = [ stdenv cmake pkgconfig hdf5 ncurses zlib mpiRuntime bluron reportinglib];
 
 
   src = fetchgitPrivate {
@@ -11,11 +21,17 @@ stdenv.mkDerivation rec {
     sha256 = "08i81msnpbxkqp4nf2yv555ybrx44hcvbm0nvgwwxc5rai8xqr00";
   };
   
+##
+## downgrade required version : bug in 2015R2 release, 2.5 do not exist
+## create defines.h file, need to be generated properly through configure_file()
   patchPhase= ''
-	ln -s ${bbp-cmake}/share/bbp-cmake/Common CMake/common &&
-	sed -i 's@include(CommonCPack)@include(PackageConfig)@g' CMakeLists.txt
-	'';  
+ln -s ${bbp-cmake}/share/bbp-cmake/Common CMake/common;
+sed -i 's@include(CommonCPack)@include(PackageConfig)@g' CMakeLists.txt;
+sed -i 's@ReportingLib 2.5@ReportingLib 2.4@g' CMake/FindPackages.cmake;
+touch neurodamus/defines.h
+    '';  
   
+    cmakeFlags="-DBluron_PREFIX_DIR=${bluron}";
 }
 
 
