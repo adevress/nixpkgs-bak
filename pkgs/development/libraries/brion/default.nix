f88aee2b4c5da9cc352ea5b5de42c32efaf46ce5
Adrien Devresse <adrien.devresse@epfl.ch>
2015-07-27 13:28:15 +0200

    Update Brion to last stable version 2015R2 (1.6.0)

--- pkgs/development/libraries/brion/default.nix
+++ pkgs/development/libraries/brion/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchgit, boost, cmake, bbp-cmake, servus, lunchbox, vmmlib, cppcheck, pkgconfig, hdf5, zlib, doxygen }:
 
 stdenv.mkDerivation rec {
-  name = "brion-1.5.0.0DEV";
+  name = "brion-1.6.0-stable";
   buildInputs = [ stdenv pkgconfig boost cmake servus lunchbox vmmlib hdf5 zlib doxygen cppcheck];
 
   src = fetchgit {
     url = "https://github.com/BlueBrain/Brion.git";
     
-    rev = "609152af51661fddc1e4df49ac644c9de24afd5e";
-    sha256 = "1rswxmcivr1vj3ikyzwlcz97292xg6siclygcj1s3rq1xwvw34f9";
+    rev = "b7a91eb9f1ecdbe1c05b8e7cfd66b7add32197a0";
+    sha256 = "1l66h3gcq7j1yy1jdcjdh0g4nxpdy91nk7phi9b8mg4q2nlgayfq";
   };
 
   enableParallelBuilding = true;
