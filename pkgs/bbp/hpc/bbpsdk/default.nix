6756a93e411c06f392588693ec876d18c34a8704
Adrien Devresse <adrien.devresse@epfl.ch>
2015-07-27 13:45:54 +0200

    Update BBP SDK to release 2015R2 version ( 0.22.0 )

--- pkgs/bbp/hpc/bbpsdk/default.nix
+++ pkgs/bbp/hpc/bbpsdk/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchgitPrivate, boost, lunchbox, brion, vmmlib, servus, cmake, bbp-cmake, mpiRuntime, pkgconfig, python, hdf5, doxygen }:
 
 stdenv.mkDerivation rec {
-  name = "bbpsdk-0.22.0.0DEV";
+  name = "bbpsdk-0.22.0-stable";
   buildInputs = [ stdenv pkgconfig boost brion vmmlib servus cmake bbp-cmake mpiRuntime lunchbox python hdf5 doxygen];
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/common/BBPSDK";
-    rev= "dbba3f6f1cc175b5d8da0376a97199e01e805573";
-    sha256 = "1218ymr4iylnipgqpdi4w1dwssrlfgyb4pn9c1anbc4gg33i468a";
+    rev= "7c0e035207d310a99cdd1b4b26fbaa1d13dd3196";
+    sha256 = "0830cgcnzdh9x699mxi388xfr3hh81ash0xgmkjd3myrv6h4w57q";
   };
 
 # disable Java bindings
