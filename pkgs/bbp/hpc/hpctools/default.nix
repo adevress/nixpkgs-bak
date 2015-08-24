778cec105ef59d10f705a39f01829f5c9d48bf2e
Adrien Devresse <adrien.devresse@epfl.ch>
2015-08-24 17:29:01 +0200

    Update HPCTools to last version, contain fix for memory corruption

--- pkgs/bbp/hpc/hpctools/default.nix
+++ pkgs/bbp/hpc/hpctools/default.nix
@@ -1,16 +1,16 @@
 { stdenv, fetchgitPrivate, boost, libxml2, cmake, bbp-cmake, mpiRuntime, pkgconfig, python, hdf5, doxygen }:
 
 stdenv.mkDerivation rec {
-  name = "hpctools-3.3.0-stable";
+  name = "hpctools-3.3.0-DEV";
   buildInputs = [ stdenv pkgconfig boost cmake mpiRuntime libxml2 python hdf5 doxygen];
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/hpc/HPCTools";
-    rev = "f6ab16612e33692ad057c5ea637a141c4a19378a";
-    sha256 = "0wbh15mnl7y4dj8g7ln6abagg9mjhkpvhlvii22r0xvmv86fzy1h";    
+    rev = "69eaff60e664a58fcce57bd673fbb2da267b18f9";
+    sha256 = "1m09ldgzvgw55r3cskcklanf7xl0il6hhzncars5gcly7bnl625r";    
   };
   
-  patchPhase= ''
+	patchPhase= ''
 	ln -s ${bbp-cmake}/share/bbp-cmake/Common CMake/common
 	'';     
   
